f4c8da7799c4e05881ff0ee25e99f2a8c633a222
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-03-13 19:50:33 -0300

    update

---
+++ src/componentes/Card/CardProduct.jsx
@@ -0,0 +1,21 @@
+
+
+const CardProduct = () => {
+    return (
+        <section>
+            <div>
+                <div>
+
+                </div>
+            </div>
+            <div>
+
+            </div>
+            
+        </section>
+    )
+        
+        
+}
+
+export default CardProduct
