00d37fc924f4c57b3d9782d84680b3d7dd19f1eb
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-03-15 23:29:35 -0300

    update

--- src/componentes/Card/CardProduct.jsx
+++ src/componentes/Card/CardProduct.jsx
@@ -1,43 +1,31 @@
 import './CardProduct.css'
 import '../DataProducts/DataCardProducts.jsx'
 import '@mantine/carousel/styles.css';
-import { Carousel } from '@mantine/carousel';
-import { MantineProvider } from '@mantine/core';
+
 // import Slider from '../Slider/Slider.js'
 
 
 const CardProduct = (props) => {
     return (
 
-        <MantineProvider>
-<Carousel 
-height={200}
-slideSize="33.333333%"
-slideGap="md"
+       
+                    <section className="container-card-header">
+                            <div className="container-card-discount">
+                                <p>{props.discount}% OFF</p>
+                                
+                            </div>
+                    
+                        <div className="container-card-img" >
+                            <img src={props.image} alt="" />
+                        </div>
 
-align="start"
-slidesToScroll={3} >
-<Carousel.Slide>
-            <section className="container-card-header">
-                    <div className="container-card-discount">
-                        <p>{props.discount}% OFF</p>
-                        
-                    </div>
-            
-                <div className="container-card-img" >
-                    <img src={props.image} alt="" />
-                </div>
-
-                <div className="container-card-footer">
-                    <p>{props.nome}</p>
-                    <p className='container-card-price'>R${props.price}</p>
-                    <img src={props.imageVariation} alt="Icone de variação de cor" />
-                </div>           
-            </section>
-        </Carousel.Slide>
-</Carousel>
-</MantineProvider>
-            
+                        <div className="container-card-footer">
+                            <p>{props.nome}</p>
+                            <p className='container-card-price'>R${props.price}</p>
+                            <img src={props.imageVariation} alt="Icone de variação de cor" />
+                        </div>           
+                    </section>
+        
         
 
     )
--- src/componentes/Slider/SliderProducts.jsx
+++ src/componentes/Slider/SliderProducts.jsx
@@ -4,27 +4,42 @@ import DataProducts from "../DataProducts/DataCardProducts";
 import CardProduct from "../Card/CardProduct";
 import { MantineProvider } from "@mantine/core";
 import '@mantine/carousel/styles.css';
+import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react';
+import { rem } from '@mantine/core';
 
 
 
-
-const Slider = () => {
+const Slider = (props) => {
     return (
         <MantineProvider>
-            <Carousel withIndicators
-      height={200}
-      slideSize="33.333333%"
-      slideGap="md"
-      align="start"
-      slidesToScroll={3} >
-                {DataProducts.map(props => <CardProduct
-                 key={props.nome}
-                 nome={props.nome}
-                 image={props.image}
-                 imageVariation={props.imageVariation}
-                 price={props.price}
-                 discount={props.discount}
-                />)}
+            <Carousel
+            key={props.nome}
+            width={288}
+             withIndicators
+             height={200}
+             slideSize="33.333333%"
+             slideGap="md"
+             loop
+             align="start"
+             slidesToScroll={3}
+             nextControlIcon={<IconArrowRight style={{ width: rem(16), height: rem(16) }} />}
+            previousControlIcon={<IconArrowLeft style={{ width: rem(16), height: rem(16) }} />}
+             >
+        
+            {DataProducts.map(props => 
+            
+            <Carousel.Slide width={288} key={props.nome}>
+                <CardProduct
+                    key={props.nome}
+                    nome={props.nome}
+                    image={props.image}
+                    imageVariation={props.imageVariation}
+                    price={props.price}
+                    discount={props.discount}
+                    /></Carousel.Slide>
+                )}
+        
+                
         
         </Carousel>
         </MantineProvider>
