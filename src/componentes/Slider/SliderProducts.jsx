14ca6738bfc4e10f736e33634bdf1294648d32b4
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-03-17 06:05:46 -0300

    update

--- src/componentes/Slider/SliderProducts.jsx
+++ src/componentes/Slider/SliderProducts.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import { Carousel } from '@mantine/carousel';
+import { Carousel, CarouselSlide } from '@mantine/carousel';
 import DataProducts from "../DataProducts/DataCardProducts";
 import CardProduct from "../Card/CardProduct";
 import { MantineProvider } from "@mantine/core";
 import '@mantine/carousel/styles.css';
-
 import { rem } from '@mantine/core';
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 
@@ -12,11 +11,11 @@ import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 
 const Slider = (props) => {
     return (
-        <MantineProvider>
+        <MantineProvider width={100} >
             <Carousel
-            key={props.nome}
-            width={288}
-             withIndicators
+             showthumbs={4}
+             key={props.nome}
+             width={100}
              height={200}
              slideSize="33.333333%"
              slideGap="md"
@@ -24,12 +23,12 @@ const Slider = (props) => {
              align="start"
              slidesToScroll={3}
              nextControlIcon={<IconArrowRight style={{ width: rem(16), height: rem(16) }} />}
-            previousControlIcon={<IconArrowLeft style={{ width: rem(16), height: rem(16) }} />}
+            previousControlIcon={<IconArrowLeft style={{ width: rem(1), height: rem(16) }} />}
              >
         
             {DataProducts.map(props => 
             
-            <Carousel.Slide width={288} key={props.nome}>
+            <CarouselSlide key={props.nome}>
                 <CardProduct
                     key={props.nome}
                     nome={props.nome}
@@ -37,7 +36,7 @@ const Slider = (props) => {
                     imageVariation={props.imageVariation}
                     price={props.price}
                     discount={props.discount}
-                    /></Carousel.Slide>
+                    /></CarouselSlide>
                 )}
         
                 
