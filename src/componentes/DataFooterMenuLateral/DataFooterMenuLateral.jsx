19edf861404a94e6e4fe0e0c0483a51980880c2b
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-04-16 04:21:42 -0300

    update

--- src/componentes/DataFooterMenuLateral/DataFooterMenuLateral.jsx
+++ src/componentes/DataFooterMenuLateral/DataFooterMenuLateral.jsx
@@ -4,7 +4,7 @@ const DataFooterMenuLateral = [
     paragraph1: "Fazer Login/Registrar",
     paragraph2: "Meus Favoritos",
     paragraph3: "Meus Pedidos",
-    icon1: "../imagens/icone.png",
+    icon1: "../imagens/icone3.png",
     icon2: "../imagens/User Dark Black.png",
     icon3: "../imagens/CoracaoBlack.png",
     icon4: "../imagens/Vector.png",
